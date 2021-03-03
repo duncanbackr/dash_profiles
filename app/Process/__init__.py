fcc9b691a719a7dbf6d203cd2ea228698f939f55
ecavan <eli_cavan@live.ca>
2021-03-03 14:08:41 -0500

    refactor backrest folder

--- app/Process/__init__.py
+++ app/Process/__init__.py
@@ -1,16 +1,16 @@
-from helpers.get_data import get_videos, get_comments, get_fans
+from Backrest.get_data import get_videos, get_comments, get_fans
 import json 
-from helpers.get_creds import exchange_jwt_for_token, create_signed_jwt
+from Backrest.get_creds import exchange_jwt_for_token, create_signed_jwt
 import pandas as pd
-from helpers.add_active_label import add_active_label
-from helpers.add_response import add_received_response_column
-from helpers.get_delays import get_delays
-from helpers.get_top_fan_cutoff import get_cutoff
-from helpers.cum_count import add_cum_count_column
-from helpers.add_passive_label import add_label, fan_type
-from helpers.get_df import videos_comment_fans_df
-from helpers.metrics import fan_metrics
-from helpers.parser import parse_timestamp
+from Process.add_active_label import add_active_label
+from Process.add_response import add_received_response_column
+from Process.get_delays import get_delays
+from Process.get_top_fan_cutoff import get_cutoff
+from Process.cum_count import add_cum_count_column
+from Process.add_passive_label import add_label, fan_type
+from Backrest.get_df import videos_comment_fans_df
+from Process.metrics import fan_metrics
+from Process.parser import parse_timestamp
 from datetime import datetime,timedelta
 import numpy as np
 
