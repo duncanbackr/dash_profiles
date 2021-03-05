046a8ad33adbcfefdad7884396489d2a81b0824e
ecavan <eli_cavan@live.ca>
2021-03-04 22:34:02 -0500

    fixed email metrics fig

--- app/Graphs/__init__.py
+++ app/Graphs/__init__.py
@@ -10,7 +10,7 @@ from plotly.subplots import make_subplots
 #from Graphs.df_active_fan_trend import activeFans_fig
 from Graphs.bubble import bubble_fig
 #from Graphs.df_day import  fig_day
-#from Graphs.df_time import df_time_auth, df_time_user, fig_time
+from Graphs.time_line import df_time_auth, df_time_user, fig_time
 #from main.graphs.df_topfan import df_topfan, fig_topfan
 # from Graphs.df_video_timeline import fig_video_timeline
 # from Graphs.df_first_month_comparison import fig_video_comparison
@@ -20,18 +20,20 @@ from Graphs.funnel import get_funnel
 from Graphs.pie_chart import get_active_label_pie
 
 
-def get_layout(df):
+def get_layout(df_user, df_auth):
 
-    fig_pie = get_active_label_pie(df)
-    fig_funnel = get_funnel(df)
-    fig_bubble = bubble_fig(df)
+    fig_pie = get_active_label_pie(df_user)
+    fig_funnel = get_funnel(df_user)
+    fig_bubble = bubble_fig(df_user)
+    fig_time_of_day = fig_time(df_user, df_auth)
 
     layout = html.Div(children=[
         html.H1(children='Creator Dashboards'),
         html.Div(children='''Analytics to help Creators grow their engagements'''),
             dcc.Graph(id='example-graph',figure=fig_pie),
-            dcc.Graph(id='example-graph',figure=fig_funnel),
-            dcc.Graph(id='example-graph',figure=fig_bubble)
+            dcc.Graph(id='example-graph2',figure=fig_funnel),
+            dcc.Graph(id='example-graph3',figure=fig_bubble),
+            dcc.Graph(id='example-graph4',figure=fig_time_of_day)
 
         ]
     )
