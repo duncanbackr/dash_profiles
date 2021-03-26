65aadbdfa83b47739e8a7296e979532c1739cdaf
duncanbackr <duncan@backr.space>
2021-03-25 23:44:26 -0400

    reduce call size to backrest

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ Create [.env](.env) as per [.env.example](.env.example) <br>
 `docker-compose down` stops the container <br>
 `pytest` runs tests <br>
 `python manage.py [command]` runs a cli commands found in [manage.py](manage.py) <br>
+
+
+connect to db
+./cloud_sql_proxy -instances=backr-dev:us-central1:data-main=tcp:5432
--- app/Backrest/__init__.py
+++ app/Backrest/__init__.py
@@ -46,7 +46,7 @@ def get_raw_data(youtube_channel_id:str):
         request.get_resource(
             resource='/v1/youtube/comments/',
             params={'youtube_video__creator__youtube_platform_account_id': youtube_channel_id,
-                'limit':15000},
+                'limit':10000},
             token=token,
         )
     )
